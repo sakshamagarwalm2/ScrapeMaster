2420a1793896524568c1395be87ab10f123528bc
Saksham Agarwal <114224728+sakshamagarwalm2@users.noreply.github.com>
2024-12-29 17:18:14 +0530

    prisma

---
+++ scrape-master/actions/workflows/getWorkflowsForUser.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export async function GetWorkflowsForUsers(){
+    const {userId}=auth();
+    if (!userId){
+        throw new Error('Unauthorized');
+    }
+
+    return prisma.workflow.findMany({
+        where:{
+            userId,
+        },
+        orderBy: {
+            createdAt: 'asc',
+        }
+    })
+}
