073939d65b08b6970cd80cbb865e89bbf91edc42
Saksham Agarwal <114224728+sakshamagarwalm2@users.noreply.github.com>
2025-01-01 21:32:09 +0530

    day end 44

--- scrape-master/type/workflow.ts
+++ scrape-master/type/workflow.ts
@@ -1,5 +1,18 @@
+import { LucideProps } from "lucide-react";
+import { TaskParam, TaskType } from "./task";
+
 export enum workflowStatus{
     DRAFT = "DRAFT",
     PUBLISHED = "PUBLISHED",
     
+}
+
+export type WorkflowTask={
+    lable: string;
+    icon: React.FC<LucideProps>;
+    type: TaskType;
+    isEntryPoint?: boolean;
+    inputs: TaskParam[];
+    output: TaskParam[];
+    credits:number;
 }
